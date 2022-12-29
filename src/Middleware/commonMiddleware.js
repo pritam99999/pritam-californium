1d926031d6a28218431d739cc53f3dd9ae9614cc
pritam99999 <pritamsalgaonkar65@gmail.com>
2022-12-29 12:18:23 +0530

    update

---
+++ src/Middleware/commonMiddleware.js
@@ -0,0 +1,52 @@
+const userModel= require("../models/userModel")
+
+const loginUser = async function (req, res,next) {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+  
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+    return res.send({status: false,msg: "the credentials don't match with any user's data",});
+    let token = jwt.sign({userId: user._id.toString(),Clan:"Uchiha",Organisation: "Akatsuki",},"secret-ring");
+    res.setHeader("x-auth-token", token);
+    res.send({ status: true, token: token });
+    next();
+  };
+
+  const updateUser = async function (req, res) {  
+    let token = req.headers["x-auth-token"];
+    if (!token) token = req.headers["x-auth-token"]; 
+    if (!token) return res.send({ status: false, msg: "token must be present" });  
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId); 
+    if (!user) {
+      return res.send("Invalid User ID");
+    }
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+    res.send({ status: updatedUser, data: updatedUser });
+  
+    next();
+};
+  
+  const deleteUser = async function (req, res) {
+    let token = req.headers["x-auth-token"];
+    if (!token) token = req.headers["x-auth-token"]; 
+    if (!token){
+    return res.send({ status: false, msg: "token must be present" });  
+  }
+  
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId); 
+    if (!user) {
+      return res.send("No such user exists");
+    }
+    let userData = req.body;
+    let deletedUser = await userModel.findOneAndDelete({ _id: userId }, userData);
+    res.send({ status: deletedUser, data: deletedUser });
+    next();
+  };
+
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteUser=deleteUser;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const mid = require("../Middleware/commonMiddleware")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/users", userController.createUser)
 
-router.post("/login", userController.loginUser)
+router.post("/login", mid.loginUser)
 
-//The userId is sent by front end
 router.get("/users/:userId", userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/Updateusers/:userId", mid.updateUser)
 
+router.put("/Deleteusers/:userId", mid.deleteUser)
 module.exports = router;
